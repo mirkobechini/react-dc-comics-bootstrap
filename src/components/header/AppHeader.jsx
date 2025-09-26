50e12e6d33e5c80095caf7a03ea134040a14492d
mirkobechini <mirkobechini@gmail.com>
2025-09-26 19:12:28 +0200

    fix: improved header for smaller screens

--- src/components/header/AppHeader.jsx
+++ src/components/header/AppHeader.jsx
@@ -9,7 +9,7 @@ export default function AppHeader() {
     return (
         <>
             <div className="container">
-                <nav className="navbar navbar-expand-lg ">
+                <nav className="navbar navbar-expand-lg pb-0">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#">
                             <img className = "img-fluid" src={header_logo} alt="DC Comics Logo"  width="80"/>
@@ -17,7 +17,7 @@ export default function AppHeader() {
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+                        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                             <ul className="navbar-nav nav-underline column-gap-4">
                                 {
                                     menu.map(item =>
