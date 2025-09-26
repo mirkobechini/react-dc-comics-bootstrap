560e39060230b485a6520478c92f23acbcc8ddf1
mirkobechini <mirkobechini@gmail.com>
2025-09-26 19:18:21 +0200

    add: partially completed main

--- src/components/main/AppMain.jsx
+++ src/components/main/AppMain.jsx
@@ -1,9 +1,30 @@
-export default function AppMain(){
+export default function AppMain() {
     return (
         <>
-        <div className="container">
-            
-        </div>
+            <div id="jumbotron" className="container-fluid py-5 ">
+
+            </div>
+            <div className="container-fluid bg-dark">
+                <div className="row">
+                    <div className="col position-relative">
+                        <span id = "floatingLabel" className="bg-primary border-primary text-light text-decoration-none text-uppercase fw-bold fs-3 px-3 py-2 position-absolute" >current series</span>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col"></div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                </div>
+                <div className="row text-center ">
+                    <div className="col my-2 py-4">
+                        <a className="bg-primary border-primary text-light text-decoration-none text-uppercase fw-bold fs-6 px-5 py-2" href="#">load more</a>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
